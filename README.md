e39ad554ba6249ed6a0a37d23d7a179b7f9256c4
Saullo Bretas Silva <saullo.silva303@gmail.com>
2019-06-12 14:45:26 -0300

    dist

--- README.md
+++ README.md
@@ -1,29 +1,4 @@
-# blog
+# Blog
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Informações
+Estou utilizando a framework Vuejs, os arquivos de html, css, js estão na pasta /dist
---
+++ dist/precache-manifest.a6a0568a2f780c32b3c9ca0708f0fc4b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fde823661527dd89d54e",
+    "url": "/js/app.be1dadc9.js"
+  },
+  {
+    "revision": "6f1b811302cc261c81b6",
+    "url": "/js/chunk-136e5447.ba191c6d.js"
+  },
+  {
+    "revision": "5e85917dffe34eea39d3",
+    "url": "/css/chunk-6149e298.bb706b39.css"
+  },
+  {
+    "revision": "5e85917dffe34eea39d3",
+    "url": "/js/chunk-6149e298.bfa6729e.js"
+  },
+  {
+    "revision": "4be4fe7858d513b73af1",
+    "url": "/css/chunk-e24e402a.95b56812.css"
+  },
+  {
+    "revision": "4be4fe7858d513b73af1",
+    "url": "/js/chunk-e24e402a.c1f596ee.js"
+  },
+  {
+    "revision": "355cf09f9de43a0c474b",
+    "url": "/css/chunk-vendors.fabeb657.css"
+  },
+  {
+    "revision": "355cf09f9de43a0c474b",
+    "url": "/js/chunk-vendors.713f90bb.js"
+  },
+  {
+    "revision": "67836b1f00ef7ed918f47e2a66eb67a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
