fdee90fa15bf8e3cb8a7c9ef17122c7cb060a0dc
Saullo Bretas Silva <saullo.silva303@gmail.com>
2019-08-12 11:48:52 -0300

    add about

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -20,6 +20,22 @@
         </v-list-tile>
       </v-list>
     </v-card>
+    <v-card class="mt-3">
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0">Sobre</h3>
+        </div>
+      </v-card-title>
+
+      <v-list>
+        <v-list-tile href="https://github.com/saullo/blog-diw" avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Github</v-list-tile-title>
+            <v-list-tile-sub-title>Visite o repositorio do projeto</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
   </div>
 </template>
 
